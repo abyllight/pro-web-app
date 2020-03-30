2a05119b1daea6061af1b2a63685c34b6c1b0da1
Abylay <abyllight@gmail.com>
2020-03-31 03:07:07 +0600

    added required fields

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,8 @@
                         <label for="inputFullName" class="col-form-label col-form-label-lg">Ф.И.О</label>
                         <input type="text" class="form-control form-control-lg"
                                id="inputFullName" name="fullName"
-                               placeholder="Касымов Адильбек Асылбекович">
+                               placeholder="Касымов Адильбек Асылбекович"
+                               required>
                     </div>
                     <div class="form-group">
                         <label for="inputCity" class="col-form-label col-form-label-lg">Город</label>
@@ -27,24 +28,27 @@
                     </div>
                     <div class="form-group">
                         <label for="inputAddress" class="col-form-label col-form-label-lg">Адрес</label>
-                        <input type="text" class="form-control form-control-lg" id="inputAddress" name="address" placeholder="Укажите улицу и номер дома">
+                        <input type="text" class="form-control form-control-lg" id="inputAddress" name="address"
+                               placeholder="Укажите улицу и номер дома" required>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="inputPhone" class="col-form-label col-form-label-lg">Мобильный телефон</label>
-                            <input type="text" class="form-control form-control-lg" id="inputPhone" name="phone" placeholder="+7 (000) 000-00-00">
+                            <input type="text" class="form-control form-control-lg" id="inputPhone" name="phone"
+                                   placeholder="+7 (000) 000-00-00" required>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="inputEmail" class="col-form-label col-form-label-lg">Электронная почта</label>
-                            <input type="email" class="form-control form-control-lg" id="inputEmail" name="email" placeholder="Электронная почта">
+                            <input type="email" class="form-control form-control-lg" id="inputEmail" name="email"
+                                   placeholder="Электронная почта" required>
                         </div>
                     </div>
                     <hr>
                     <h2 class="mb-3 mt-5">2. Заявление</h2>
                     <div class="form-group">
                         <label for="inputSupervision" class="col-form-label col-form-label-lg">Выберите надзор</label>
-                        <select id="inputSupervision" class="form-control form-control-lg" name="supervision">
+                        <select id="inputSupervision" class="form-control form-control-lg" name="supervision" required>
                             <option selected>Не выбрано</option>
                             <option>Досудебное производство</option>
                             <option>Уголовное судопроизводство</option>
@@ -59,31 +63,33 @@
 
                     <div class="form-group">
                         <label for="inputCaseNumber" class="col-form-label col-form-label-lg">Номер уголовного дела (Не обязательно)</label>
-                        <input type="text" class="form-control form-control-lg" id="inputCaseNumber" name="caseNumber" placeholder="12345678">
+                        <input type="text" class="form-control form-control-lg" id="inputCaseNumber" name="caseNumber"
+                               placeholder="12345678">
                     </div>
 
                     <div class="form-group">
                         <label for="inputAuthority" class="col-form-label col-form-label-lg">Орган в производстве, которого находятся материалы</label>
-                        <input type="text" class="form-control form-control-lg" id="inputAuthority" name="authority" placeholder="МВД РК">
+                        <input type="text" class="form-control form-control-lg" id="inputAuthority" name="authority"
+                               placeholder="МВД РК" required>
                     </div>
 
                     <div class="form-group">
                         <label for="inputAgree" class="col-form-label col-form-label-lg">Не согласен с</label>
                         <textarea class="form-control form-control-lg" id="inputAgree" name="agree" rows="5"
-                                  placeholder="Прекращением уголовного дела в отношении Иванова И."></textarea>
+                                  placeholder="Прекращением уголовного дела в отношении Иванова И." required></textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="inputAction" class="col-form-label col-form-label-lg">Какие меры просите принять?</label>
                         <textarea class="form-control form-control-lg" id="inputAction" name="action" rows="5"
-                                  placeholder="Прошу отменить принятое решение и направить дело в суд"></textarea>
+                                  placeholder="Прошу отменить принятое решение и направить дело в суд" required></textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="inputSolve" class="col-form-label col-form-label-lg">
                             Обращались ли Вы в иные организации или суды для решения указанных вопросов?
                         </label>
-                        <textarea class="form-control form-control-lg" id="inputSolve" name="solve" rows="5"></textarea>
+                        <textarea class="form-control form-control-lg" id="inputSolve" name="solve" rows="5" required></textarea>
                     </div>
                     {{--<div class="form-group">
                         <label for="file" class="col-form-label col-form-label-lg">Прикрепите файлы</label>
